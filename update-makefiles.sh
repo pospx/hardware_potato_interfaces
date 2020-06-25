a5e2c6a186150bfccc286e8f41f4dedf5e607a94
Raj Jadeja <rajsjadeja4@gmail.com>
2020-06-25 18:34:28 +0000

    adapt for potato

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/aosip/interfaces" \
+  "vendor.lineage:hardware/potato/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
